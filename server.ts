2068544830434f2d59faefa415ead8c4302598e8
LucFERRO <luchenri.ferro@gmail.com>
2022-09-29 15:34:43 +0200

    small fix

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ import {Response, Request} from 'express'
 app.use(express.json())
 
 // Pour créer DB, à commenter sinon
-// sequelize.initDb()
+sequelize.initDb()
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
