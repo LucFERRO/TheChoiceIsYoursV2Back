d98a8bee2939d980480b3d386224bd8d0b47e409
LucFERRO <luchenri.ferro@gmail.com>
2022-09-29 16:18:33 +0200

    new mocks

--- database/mock-user.ts
+++ database/mock-user.ts
@@ -1,30 +1,30 @@
 const users = [
   {
-    username: "Test",
-    password: "1234",
-    firstname: "Test",
-    lastname: "Gaet",
-    date_of_birth: "2004-12-31T23:00:00.000Z",
-    email: "gaetan@gaetan.fr",
-    biography: null,
-    profile_picture: null
-  },
-  {
-    username: "Gaetan2",
-    email: "gaetan2@gaetan.fr",
+    username: "Zul",
+    email: "luc@f.fr",
     password: "$2b$10$KagIgrzOn3GYFbvgA8WUluUNmOpYmwbCQoyVYNz7f4PNQV.Bwf2aS",
-    firstname: "Gaetan",
-    lastname: "Gaet",
+    firstname: "Luc",
+    lastname: "FERRO",
     date_of_birth: "2004-12-31T23:00:00.000Z",
     biography: null,
     profile_picture: null
   },
   {
-    username: "Luc",
-    email: "luc@luc.fr",
+    username: "Sakou",
+    email: "luc@v.fr",
     password: "$2b$10$KagIgrzOn3GYFbvgA8WUluUNmOpYmwbCQoyVYNz7f4PNQV.Bwf2aS",
-    firstname: "Gaetan",
-    lastname: "Gaet",
+    firstname: "Luc",
+    lastname: "Vigneron",
+    date_of_birth: "2004-12-31T23:00:00.000Z",
+    biography: null,
+    profile_picture: null
+  },
+  {
+    username: "Astroweed",
+    email: "proton@proton.fr",
+    password: "$2b$10$KagIgrzOn3GYFbvgA8WUluUNmOpYmwbCQoyVYNz7f4PNQV.Bwf2aS",
+    firstname: "Alexis",
+    lastname: "Cuvillier",
     date_of_birth: "2004-12-31T23:00:00.000Z",
     biography: null,
     profile_picture: null
