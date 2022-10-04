ea8f3c841eb8c007faecc87a0a806a1371f07a16
LucFERRO <luchenri.ferro@gmail.com>
2022-10-04 15:23:29 +0200

    refresh tokens in db

--- database/mock-token.ts
+++ database/mock-token.ts
@@ -2,6 +2,9 @@ const tokens = [
   {
     refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiWnVsIiwiaWF0IjoxNjY0Nzg2ODMyfQ.oXdMRVtXuigBJhQics70gaoMohXmK4bYIGZG-yUrstA",
   },
+  {
+    refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2Frb3UiLCJpYXQiOjE2NjQ4ODkxMTN9.gYJnG68z8Rf8ZkP0260LYxm3ILv_oMZNCZ2o1U1uGV0",
+  },
 ];
 
 module.exports = tokens;
--- routes/auth/token.ts
+++ routes/auth/token.ts
@@ -2,12 +2,11 @@ import { Application } from "express";
 import { ValidationError } from "sequelize";
 import { ApiException } from "../../types/exception";
 import { userTypes } from "../../types/user";
+import { tokenTypes } from "../../types/token";
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const { User } = require('../../database/connect')
-
-let refreshTokens : any = ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiWnVsIiwiaWF0IjoxNjY0Nzg2ODMyfQ.oXdMRVtXuigBJhQics70gaoMohXmK4bYIGZG-yUrstA']
+const { User, Token } = require('../../database/connect')
 
 /**
   * @openapi
@@ -31,11 +30,22 @@ module.exports = (app: Application) => {
     app.post("/api/auth/token", (req, res) => {
     const refreshToken = req.body.token
     if (refreshToken == null) return res.sendStatus(401)
-    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err : Error, user : any) => {
-        if (err) return res.sendStatus(403)
-        const accessToken = jwt.sign({name: user.username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'})
-        res.json({accessToken: accessToken})
+
+    Token.findAll()
+    .then((tokens: any) => {
+        let refreshTokens : any = []
+
+        tokens.map((token : tokenTypes) => {
+            refreshTokens.push(token.refreshToken)
+        })
+
+        console.log('All tokens',refreshTokens)
+        if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err : Error, user : any) => {
+            if (err) return res.sendStatus(403)
+            const accessToken = jwt.sign({name: user.username}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'})
+            res.json({accessToken: accessToken})
+            })
         })
     })
 };
